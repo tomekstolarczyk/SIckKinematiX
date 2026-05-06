76d01627458e1f975254bd48a56ef54c83d3f6c5
tomekstolarczyk <tomasz.d.stolarczyk@gmail.com>
2026-05-06 02:10:21 +0200

    function: create MDH Transform Matrix

--- kinematics.h
+++ kinematics.h
@@ -5,11 +5,11 @@ typedef struct {
     double data[16];
 } Matrix44;
 
-void createIdentityMatrix(Matrix44* M);
+void create4x4IdentityMatrix(Matrix44* M);
+
+void createMDHTransformMatrix(double a, double alpha, double d, double theta, Matrix44* result);
 
 void multiply4x4Matrix(Matrix44* M1, Matrix44* M2, Matrix44* result);
-
-void createMDHParametersMatrix(double a, double alpha, double d, double theta, Matrix44* result);
 
 void ForwardKinematics(const double thetas[6], Matrix44* result);
 
