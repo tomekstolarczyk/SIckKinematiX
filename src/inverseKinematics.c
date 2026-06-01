081ae44cd6fbf8dd4074b92f246082be095f152f
tomekstolarczyk <tomasz.d.stolarczyk@gmail.com>
2026-06-02 00:46:48 +0200

    DLS should be full working now

--- src/inverseKinematics.c
+++ src/inverseKinematics.c
@@ -81,7 +81,7 @@ void inverseKinematicsDLS(RobotArm6DoF* ramie, double* thetas, const Matrix44* t
     for (int i = 0; i < max_iters; i++)
     {
         // 1. where are we now?
-        forwardKinematics(ramie, thetas, &fkResults);
+        forwardKinematics(ramie, thetas, fkResults);
         Matrix44* currentPose = &(fkResults[5]);
 
         // 2.1 error for xyz 
@@ -90,8 +90,34 @@ void inverseKinematicsDLS(RobotArm6DoF* ramie, double* thetas, const Matrix44* t
         Vector3D xyzErrorVector;
         myVectorSub(&targetXYZ, &currentXYZ, &xyzErrorVector);
 
-        // 2.2 error for rotation
-        Vector3D rotationErrorVector;
+        // 2.2 error for rotation (using the Luh-Walker-Paul method)
+
+        // os obrotu X
+        Vector3D currentRotationX = {currentPose->data[0], currentPose->data[4], currentPose->data[8]};
+        Vector3D targetRotationX = {targetPose->data[0], targetPose->data[4], targetPose->data[8]};
+        Vector3D crossX;
+        myVectorCrossProduct(&currentRotationX,&targetRotationX,&crossX);
+
+        // os obrotu Y
+        Vector3D currentRotationY = {currentPose->data[1], currentPose->data[5], currentPose->data[9]};
+        Vector3D targetRotationY = {targetPose->data[1], targetPose->data[5], targetPose->data[9]};
+        Vector3D crossY;
+        myVectorCrossProduct(&currentRotationY,&targetRotationY,&crossY);
+
+        // os obrotu Z
+        Vector3D currentRotationZ = {currentPose->data[2], currentPose->data[6], currentPose->data[10]};
+        Vector3D targetRotationZ = {targetPose->data[2], targetPose->data[6], targetPose->data[10]};
+        Vector3D crossZ;
+        myVectorCrossProduct(&currentRotationZ,&targetRotationZ,&crossZ);
+        
+        // finalny rotation error vector
+        Vector3D rotationErrorVector = {
+            0.5 * (crossX.x + crossY.x + crossZ.x),
+            0.5 * (crossX.y + crossY.y + crossZ.y),
+            0.5 * (crossX.z + crossY.z + crossZ.z)
+        };
+
+        // TODO PATCH: LWP nie wylapuje bledu obrotu 180 stopni (possible rozwiazanie : Trace) 
 
         // 3. are we on target?
         double xyzErrorValue = myVectorLength(&xyzErrorVector);
@@ -105,20 +131,23 @@ void inverseKinematicsDLS(RobotArm6DoF* ramie, double* thetas, const Matrix44* t
         // 5. invert Jacobian - find DLS coeff
         if(calculateDLSInverse(&J, lambda, &J_inv_DLS) == 0){break;}
         
-        // 6. find delta theta
+        // 6. find delta thetas
         // delta theta = J_inv_DLS * deltaX
         double deltaX[] = {xyzErrorVector.x, xyzErrorVector.y, xyzErrorVector.z, rotationErrorVector.x, rotationErrorVector.y,rotationErrorVector.z};
-        double deltaTheta[6];
-
-        
-
+        double deltaThetas[6] = {0};
+        for(size_t r = 0; r < 6; r++)
+        {
+            for(size_t c = 0; c<6; c++)
+            {
+                deltaThetas[r] += J_inv_DLS.data[6*r+c]*deltaX[c];
+            }
+        }
 
         // 7. udpate thetas with step size
         for(size_t i = 0; i<6; i++)
         {
-            thetas[i] += deltaTheta[i]*step_size;
+            thetas[i] += deltaThetas[i]*step_size;
             thetas[i] = atan2(sin(thetas[i]), cos(thetas[i])); // normalize angles
         }
-
     }
 }
