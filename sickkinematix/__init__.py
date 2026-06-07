1394572af947d33a85962ab45c8ca989dfc43bba
tomekstolarczyk <tomasz.d.stolarczyk@gmail.com>
2026-06-07 19:30:21 +0200

    nothing much

--- sickkinematix/__init__.py
+++ sickkinematix/__init__.py
@@ -1,4 +1,2 @@
-# sickkinematix/__init__.py
 from .c_kinematix import build_robot, fk, ik_ccd, ik_dls, workspace, yoshikawa
-
 __all__ = ["build_robot", "fk", "ik_ccd", "ik_dls", "workspace", "yoshikawa"]
