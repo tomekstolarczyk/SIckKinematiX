275a666ebd751fc38728af5a480cbe613f6c4107
tomekstolarczyk <tomasz.d.stolarczyk@gmail.com>
2026-06-10 23:15:38 +0200

    openmp for linux only for now

--- setup.py
+++ setup.py
@@ -5,26 +5,21 @@ import sys
 compile_args = []
 link_args = []
 
-if sys.platform.startswith("win"):
-    compile_args = ["/openmp"]
-elif sys.platform.startswith("linux"):
+if sys.platform.startswith("linux"):
+    # Linux zostaje z pełną mocą OpenMP
     compile_args = ["-fopenmp"]
     link_args = ["-lgomp"]
-elif sys.platform == "darwin":
-    # Na macOS wyłączamy flagi OpenMP dla świętego spokoju.
+else:
+    # Windows i macOS - fallback do 1 wątku dla stabilności budowania
     compile_args = []
     link_args = []
 
 module = setuptools.Extension(
     "sickkinematix.c_kinematix", 
     sources=[
-        "src/pythonWrapper.c",
-        "src/robot.c",
-        "src/justMath.c",
-        "src/forwardKinematics.c",
-        "src/inverseKinematics.c",
-        "src/workspaceAnalysis.c",
-        "src/jacobian.c"
+        "src/pythonWrapper.c", "src/robot.c", "src/justMath.c",
+        "src/forwardKinematics.c", "src/inverseKinematics.c",
+        "src/workspaceAnalysis.c", "src/jacobian.c"
     ],
     include_dirs=[numpy.get_include()],
     extra_compile_args=compile_args,
